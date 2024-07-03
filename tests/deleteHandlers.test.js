864e7aaf943ab5a182856bf7b2752d59f2197b13
Graham Mason <grahamtmason@me.com>
2024-07-03 00:14:00 -0400

    DELETE (spaces and tabs correction)v3

--- tests/deleteHandlers.test.js
+++ tests/deleteHandlers.test.js
@@ -2,30 +2,30 @@
 const config = require('../config');
 
 test('status code should be 404', async () => {
-	let actualStatus;
-	try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/7`);
-	actualStatus = response.status;
-	} catch (error) {
-		console.error(error);
-	}
-	expect(actualStatus).toBe(404);
-  });
+let actualStatus;
+try {
+const response = await fetch(`${config.API_URL}/api/v1/kits/7`);
+actualStatus = response.status;
+} catch (error) {
+console.error(error);
+}
+expect(actualStatus).toBe(404);
+});
 
 test('delete kit', async () => {
-    try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
-			method: 'DELETE',
-		});
-		const contentType = response.headers.get('content-type');
-		let actualResponseBody;
-		if (contentType && contentType.includes('application/json')) {
-		  actualResponseBody = await response.json();
-		} else {
-		  actualResponseBody = await response.text();
-		}
-		expect(actualResponseBody).toEqual({ ok: true });
-	  } catch (error) {
-		console.error('Error:', error);
-	  }
-	});
+	try {
+const response = await fetch(`${config.API_URL}/api/v1/kits/7`, {
+method: 'DELETE',
+});
+const contentType = response.headers.get('content-type');
+let actualResponseBody;
+if (contentType && contentType.includes('application/json')) {
+actualResponseBody = await response.json();
+} else {
+actualResponseBody = await response.text();
+}
+expect(actualResponseBody).toEqual({ ok: true });
+} catch (error) {
+console.error('Error:', error);
+}
+});
